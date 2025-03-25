573d79e416cd27f4415646c8582dcdf252748db5
sccsc1342025 <sebastiancam711@gmail.com>
2025-03-25 20:23:35 +0000

    csc134_M4HW1.cpp

---
+++ csc134_M4HW1.cpp
@@ -0,0 +1,18 @@
+// CSC 134
+// M4HW1 Gold
+// Sebastian Camacho
+// 03/25/2025
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int i = 1;
+    while (i <= 12)
+    {
+        cout << "5 times " << i << " is " << 5 * i << endl;
+        i++;
+    }
+    
+    return 0;
